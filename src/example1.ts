90507396a57539b53efb63cdb5ae03bf04372be4
jhyang <jihun202@sk.com>
2021-10-20 19:54:38 +0900

    .

--- src/example1.ts
+++ src/example1.ts
@@ -1,35 +1,9 @@
 /*
  * Example 1
- * Basic Types 에 대해 작성 해보겠습니다.
+ * Basic Types, Literal, Function 에 대해 작성 해보겠습니다.
  */
 
-// Boolean
-
-// Number
-
-// String
-
-// Null
-
-// Undefined
-
-// Symbol
-
-// Array: object
-
-// Any
-
-// Void
-
-// Never
-
-// Enum
-
-// Tuple: object
-
-/*
- * Initialize
- */
+// Basic Types
 
 // Literal
 
