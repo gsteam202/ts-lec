fa123c94ce75d292c98d181b52f464c455625a04
jhyang <jihun202@sk.com>
2021-10-23 18:31:17 +0900

    .

--- src/example4.js
+++ src/example4.js
@@ -1,3 +1,11 @@
+/*
+ * Example 4
+ * js 파일을 ts 로 rename 하세요.
+ * bankAccount args 와 return value 에 대한 interface 를 정의하세요.
+ * tsc 로 작성한 ts 파일을 compile 하세요.
+ * node 커맨드로 compiled 된 js 파일을 실행 하세요.
+ */
+
 const bankAccount = (initialBalance) => {
   let balance = initialBalance;
 
@@ -14,5 +22,6 @@ const bankAccount = (initialBalance) => {
 
 const account = bankAccount(100);
 
-account.getBalance(); // 100
-account.deposit(10); // 110
+console.log(account.getBalance()); // 100
+console.log(account.deposit(10)); // 110
+console.log('실습 종료!');
